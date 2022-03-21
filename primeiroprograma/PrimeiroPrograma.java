f3522b7280689350b382ae2a28b9df0346d260e0
Michele <michele.jeniffer.mr@gmail.com>
2022-03-21 14:33:22 -0300

    primeira aula

---
+++ primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,34 @@
+package primeiroprograma;//teste
+//packgae = pacote 
+//todas letras minusculas é um package
+//todas letras maiusculas é uma constante
+
+public class PrimeiroPrograma {
+//class = classe
+//técnica caixa do camelo (sensitive)
+//primeira letra em maiusculo de cada palavara indica uma classe ou interface
+//primeira minuscula e demais primeira maiuscula (atributo, variavel, parametro, método)
+	
+	public static void main(String[] args) {
+		//void = vazio
+		//main = principal
+		/*um método principal que está dentro da classe "Primeiro Programa"
+		 *  que faz parte do pacote "Primeiro Programa"
+		 */
+		
+		
+		System.out.print("Olá, mundo!!!");
+		//system = sistema
+		//out = saída
+		//print = imprima ou mostre
+		
+		System.out.println("Olá, mundo!!!");
+		// sysout ou syso + ctrl + espaço
+	
+		
+		
+				
+
+	}
+
+}
